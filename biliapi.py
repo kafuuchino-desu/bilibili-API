8afad868275d34d7b8960f9886cb68d71cbd9999
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2018-12-27 13:32:33 +0800

    init

---
+++ biliapi.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+class bilivid():
+  def __init__(self, aid):
+    raw = requests.get('https://api.bilibili.com/x/web-interface/view/detail?aid=' + str(aid))
+    self.__INFO = json.loads(raw.text)
+
+  @property
+  def avid(self):
+    return self.__INFO['data']['View']['aid']
+
+  @property
+  def title(self):
+    return self.__INFO['data']['View']['title']
+
+  @property
+  def parts(self):
+    return self.__INFO['data']['View']['videos']
+
+  @property
+  def date(self):
+    return self.__INFO['data']['View']['pubdate']
+
+  @property
+  def desc(self):
+    return self.__INFO['data']['View']['desc']
+
+  @property
+  def upname(self):
+    return self.__INFO['data']['View']['owner']['name']
